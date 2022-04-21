a12dd30966188617c1499de69b6dc98070564de9
Ben McKibben <dev@trash.house>
2022-04-21 14:17:05 -0500

    wooo a loop

--- discord_phone_proxy/prototype/bot.py
+++ discord_phone_proxy/prototype/bot.py
@@ -1,4 +1,5 @@
-from asyncio import sleep
+from asyncio import sleep, gather
+from time import perf_counter
 
 import discord
 from starlette.config import Config
@@ -10,6 +11,38 @@ VC_ID = config("VC_ID")
 
 
 class MyClient(discord.Client):
+    async def output_audio_loop(self, channel):
+        FRAME_LENGTH = 20
+        DELAY = FRAME_LENGTH / 1000.0
+
+        while True:
+            i = 0
+            self.no_audio = discord.FFmpegOpusAudio("no.wav")
+            
+            start = perf_counter()
+            loops = 0
+
+            while True:
+                loops += 1
+                chunk =  self.no_audio.read()
+                if not chunk:
+                    break
+
+                self.voice_client.send_audio_packet(chunk, encode=False)
+
+                next_time = start + DELAY * loops
+                delay = max(0, DELAY + (next_time - perf_counter()))
+                await sleep(delay)
+
+    
+            await channel.send("Just finished playing audio")
+            await sleep(3)
+
+    async def still_connected_loop(self, channel):
+        while True:
+            await channel.send("Still connected")
+            await sleep(1)
+
     async def on_ready(self):
         print('We have logged in as {0.user}'.format(client))
 
@@ -29,9 +62,7 @@ class MyClient(discord.Client):
             self.vc: discord.VoiceChannel = await self.fetch_channel(VC_ID)
             self.voice_client: discord.VoiceClient = await self.vc.connect()
 
-            while True:
-                await sleep(1)
-                await message.channel.send("Still connected")
+            await gather(self.output_audio_loop(message.channel), self.still_connected_loop(message.channel))
 
 
 if __name__ == "__main__":
