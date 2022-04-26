5967d9091c19b3058c1ebaea283eccc2ab587dcc
Ben McKibben <dev@trash.house>
2022-04-25 20:17:33 -0500

    cleaning up

--- discord_phone_proxy/prototype/app.py
+++ discord_phone_proxy/prototype/app.py
@@ -1,20 +1,14 @@
 import asyncio
 import base64
 import io
-from subprocess import call
-import threading
 import wave
 from collections import deque
 
 from aiohttp import ClientSession
 from starlette.applications import Starlette
 from starlette.config import Config
-from starlette.requests import Request
-from starlette.responses import PlainTextResponse
-from starlette.routing import Route, WebSocketRoute
+from starlette.routing import WebSocketRoute
 from starlette.websockets import WebSocket
-
-from discord_phone_proxy.prototype.bot import MyClient
 
 
 config = Config(".env")
@@ -22,29 +16,27 @@ config = Config(".env")
 BOT_TOKEN = config("BOT_TOKEN")
 CHANNEL_ID = config("CHANNEL_ID")
 
-call_bytes_buffer = deque()
+AUDIO_BUFFER = deque()
 
 
-async def send_discord_message(message: str):
+async def send_discord_message(message: str) -> str:
     async with ClientSession() as session:
         async with session.post(
             f"https://discord.com/api/channels/{CHANNEL_ID}/messages",
-            json={"content": f"```json\n{message}\n```"},
+            json={"content": message},
             headers={
                 "Authorization": f"Bot {BOT_TOKEN}",
-                "User-Agent": "ProxyBot (http://some.url, v0.1)",
+                "User-Agent": "ProxyBot (http://some.url, v0.1)",  # dummy values
             },
         ) as response:
             return await response.text()
 
 
-def index(request: Request) -> PlainTextResponse:
-    return PlainTextResponse("Hiya")
-
-
-async def echo(websocket: WebSocket) -> None:
+async def phone_connect(websocket: WebSocket) -> None:
     await websocket.accept()
-    await send_discord_message("A new call has been accepted!")
+    await send_discord_message(
+        "A call has come in, use `$connect` to connect them to the voice channel!"
+    )
 
     try:
         while True:
@@ -54,7 +46,10 @@ async def echo(websocket: WebSocket) -> None:
                 # audio/x-mulaw with a sample rate of 8000 and base64 encoded
                 # Per https://www.twilio.com/docs/voice/twiml/stream#message-media-to-twilio
                 snippet = payload["media"]["payload"]
-                call_bytes_buffer.append(base64.b64decode(snippet))
+                AUDIO_BUFFER.append(base64.b64decode(snippet))
+
+                # Echo back the packet we just received, resulting in the caller
+                # talking to themselves.
                 await websocket.send_json(
                     {
                         "event": "media",
@@ -65,37 +60,28 @@ async def echo(websocket: WebSocket) -> None:
                     }
                 )
     finally:
+        # FIXME: don't think this is the right way to handle this.
         await websocket.close()
 
 
-async def discord_endpoint(websocket: WebSocket) -> None:
+async def discord_connect(websocket: WebSocket) -> None:
     await websocket.accept()
 
-    # Send wav headers.
-    wav_buffer = io.BytesIO()
-    with wave.open(wav_buffer, "wb") as f:
-        f.setnchannels(1)
-        f.setsampwidth(1)
-        f.setframerate(8000)
-
-    wav_buffer.seek(0)
-    # await websocket.send_bytes(wav_buffer.read())
-
-    call_bytes_buffer.clear()
+    # Clear out any unconsumed packets from the buffer so that
+    # Discord gets the earliest audio AFTER connection.
+    AUDIO_BUFFER.clear()
 
     while True:
         try:
-            chunk = call_bytes_buffer.popleft()
-            # print("Sending from buffer!")
+            chunk = AUDIO_BUFFER.popleft()
             await websocket.send_bytes(chunk)
         except IndexError:
-            # print("Nothing in the buffer :(")
-            await asyncio.sleep(0.02)
+            # Unblock the event loop.
+            await asyncio.sleep(0)
 
 
 routes = [
-    Route("/", index),
-    WebSocketRoute("/echo", echo),
-    WebSocketRoute("/discord", discord_endpoint),
+    WebSocketRoute("/phone", phone_connect),
+    WebSocketRoute("/discord", discord_connect),
 ]
 app = Starlette(debug=True, routes=routes)
--- discord_phone_proxy/prototype/bot.py
+++ discord_phone_proxy/prototype/bot.py
@@ -12,74 +12,49 @@ config = Config(".env")
 
 BOT_TOKEN = config("BOT_TOKEN")
 VC_ID = config("VC_ID")
+PROXY_SERVER_URL = config("PROXY_SERVER_URL", default="ws://127.0.0.1:8000/discord")
 
 
-class MyClient(discord.Client):
-    async def connect_to_vc(self):
-        self.vc: discord.VoiceChannel = await self.fetch_channel(VC_ID)
-        self.voice_client: discord.VoiceClient = await self.vc.connect()
+class ProxyClient(discord.Client):
+    async def connect_to_voice_channel(self):
+        voice_channel: discord.VoiceChannel = await self.fetch_channel(VC_ID)
+        voice_client: discord.VoiceClient = await voice_channel.connect()
 
-        async with AsyncExitStack() as stack:
-            self.call_ws = await stack.enter_async_context(
-                websockets.connect("ws://127.0.0.1:8000/discord")
-            )
-            self.call_ws_closer = stack.pop_all().aclose
+        # Okay, don't ask me how this works but we're basically constructing
+        # a pipe from the phone call to ffmpeg to Discord. The first step to
+        # doing this is to pass subprocess.PIPE in as the source.
+        audio_pipe = discord.FFmpegOpusAudio(subprocess.PIPE, pipe=True)
 
-    async def receive_wav_file(self):
-        with wave.open("call2.wav", "wb") as f:
+        with wave.open(audio_pipe._process.stdin, "wb") as f:
+            # Send wav headers to ffmpeg.
             f.setnchannels(1)
             f.setsampwidth(1)
             f.setframerate(8000)
-            while True:
-                snippet = await self.call_ws.recv()
-                f.writeframes(snippet)
+            f.setnframes(14400000)  # 30 minute time limit :(
 
-    async def output_audio_loop(self):
-        FRAME_LENGTH = 20
-        DELAY = FRAME_LENGTH / 1000.0
+            # For some reason, this works. ffmpeg will convert the audio
+            # snippets as they come in and emit silence otherwise.
+            voice_client.play(audio_pipe)
 
-        no_audio = discord.FFmpegOpusAudio(subprocess.PIPE, pipe=True)
+            async with websockets.connect(PROXY_SERVER_URL) as ws:
+                while True:
+                    snippet = await ws.recv()
 
-        with wave.open(no_audio._process.stdin, "wb") as f:
-            f.setnchannels(1)
-            f.setsampwidth(1)
-            f.setframerate(8000)
-            f.setnframes(1000000)
-
-            self.voice_client.play(
-                no_audio, after=lambda e: print(f"Player error: {e}") if e else None
-            )
-            while True:
-                snippet = await self.call_ws.recv()
-                f.writeframesraw(snippet)
-
-    async def still_connected_loop(self, channel):
-        while True:
-            await channel.send("Still connected")
-            await sleep(1)
+                    # We can't use .writeframes() here as an Illegal seek
+                    # to rewrite the header will happen.
+                    f.writeframesraw(snippet)
 
     async def on_ready(self):
-        print(f"We have logged in as {self.user}")
+        print(f"Logged in as {self.user}")
 
     async def on_message(self, message):
         if message.author == client.user:
             return
 
-        if message.content.startswith("$hello"):
-            await message.channel.send("Hello!")
-
-        if message.content.startswith("$play"):
-            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("no.wav"))
-            self.voice_client.play(
-                source, after=lambda e: print(f"Player error: {e}") if e else None
-            )
-            await message.channel.send("Playing")
-
         if message.content.startswith("$connect"):
-            await self.connect_to_vc()
-            await self.output_audio_loop()
+            await self.connect_to_voice_channel()
 
 
 if __name__ == "__main__":
-    client = MyClient()
+    client = ProxyClient()
     client.run(BOT_TOKEN)
